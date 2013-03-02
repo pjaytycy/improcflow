167c601184a1863748c7796fa2d8d4fe58ba3cb8
Pieter-Jan Busschaert <pieterjan.busschaert+github@gmail.com>
2013-03-02 21:34:11 +0100

    refactor models.py
    Big refactoring in models.py, to make things actually "work"
    Some changes in tests.py:
    => add title attribute to all elements and connections (in test1 only)
    => InputImage gets the initial data via set_value() instead of the
    constructor
    => for the result, we call element_output.result() instead of
    element_output.number

--- models.py
+++ models.py
@@ -1,33 +1,143 @@
 import numpy
 
+DEBUG = False
+
 from django.db import models
 
-class InputImage(models.Model):
-  image = None
-  
-  def __init__(self, src):
-    self.image = src
+def combine_title(part1, part2):
+  if part1 is None:
+    return None
+  if part2 is None:
+    return None
+  return str(part1) + "." + str(part2)
 
-class OpenCVMean(models.Model):
-  src = None
-  mean = None
+
+class Connector(object):
+  def __init__(self, title = None):
+    self.value = None
+    self.valid = False
+    self.title = title
+    
+  def set_value(self, value):
+    self.value = value
+    self.valid = True
   
-class OutputNumber(models.Model):
-  number = 3.5
+  def is_ready(self):
+    return self.valid
+
+    
+class Element(object):
+  def __init__(self, title = None):
+    self.title = title
+    self.input_connectors = []
+    self.output_connectors = []
+    
+  def add_input_connector(self, title = None):
+    input_connector = Connector(title = combine_title(self.title, title))
+    self.input_connectors.append(input_connector)
+    return input_connector
   
-class Flow(models.Model):
-  def addElement(self, element):
-    if type(element) == InputImage:
-      self.src = element.image
-    if type(element) == OutputNumber:
-      self.dst = element
+  def add_output_connector(self, title = None):
+    output_connector = Connector(title = combine_title(self.title, title))
+    self.output_connectors.append(output_connector)
+    return output_connector
   
-  def connect(self, src, dst):
-    pass
+  def replace_input_connector(self, old_input_connector, new_input_connector):
+    self.input_connectors.remove(old_input_connector)
+    self.input_connectors.append(new_input_connector)
+    return new_input_connector
   
+  def replace_output_connector(self, old_output_connector, new_output_connector):
+    self.output_connectors.remove(old_output_connector)
+    self.output_connectors.append(new_output_connector)
+    return new_output_connector
+    
   def run(self):
-    result = numpy.average(self.src)
-    self.dst.number = result
+    if DEBUG:
+      print "%s %s run" % (self.__class__.__name__, self.title)
+  
+  def is_ready(self):
+    for input_connector in self.input_connectors:
+      if not(input_connector.is_ready()):
+        return False
+    return True
+    
+  
+class Connection(Element):
+  def __init__(self, title = None):
+    super(Connection, self).__init__(title = title)
+    self.src = self.add_input_connector()
+    self.dst = self.add_output_connector()
+  
+  def set_src_dst(self, src, dst):
+    self.src = self.replace_input_connector(self.src, src)
+    self.dst = self.replace_output_connector(self.dst, dst)
+        
+  def run(self):
+    super(Connection, self).run()
+    if DEBUG:
+      print "  Connection %s from %s to %s" % (self.title, self.src.title, self.dst.title)
+    self.dst.set_value(self.src.value)
+
+    
+class InputImage(Element):
+  def __init__(self, title = None):
+    super(InputImage, self).__init__(title = title)
+    self.image = self.add_input_connector(title = "image")
+
+  def set_value(self, src):
+    self.image.set_value(src)
+    
+    
+class OpenCVMean(Element):
+  def __init__(self, title = None):
+    super(OpenCVMean, self).__init__(title = title)
+    self.src = self.add_input_connector(title = "src")
+    self.mean = self.add_output_connector(title = "mean")
+    
+  def run(self):
+    super(OpenCVMean, self).run()
+    self.mean.set_value(numpy.average(self.src.value))
+    
+  
+class OutputNumber(Element):
+  def __init__(self, title = None):
+    super(OutputNumber, self).__init__(title = title)
+    self.number = self.add_input_connector(title = "number")
+      
+  def result(self):
+    return self.number.value
+    
+  
+class Flow(object):
+  def __init__(self):
+    self.elements = []
+  
+  def addElement(self, element):
+    self.elements.append(element)
+  
+  def connect(self, src, dst, title = None):
+    connection = Connection(title = title)
+    connection.set_src_dst(src, dst)
+    self.elements.append(connection)
+  
+  def run(self, elements_to_do = None):
+    if elements_to_do is None:
+      elements_to_do = self.elements[:]
+    
+    elements_left = []
+    elements_done = 0
+    for element in elements_to_do:
+      if element.is_ready():
+        element.run()
+        elements_done += 1
+      else:
+        elements_left.append(element)
+    
+    if elements_done == 0:
+      return True
+    
+    return self.run(elements_left)
   
   
 ############
--- tests.py
+++ tests.py
@@ -4,24 +4,26 @@ from improcflow.models import *
 
 class FlowModelTests(TestCase):
   def test_mean_1(self):
-    element_input = InputImage([[1, 2, 3], [4, 5, 6]])
-    element_mean = OpenCVMean()
-    element_output = OutputNumber()
+    element_input = InputImage(title = "element_input")
+    element_input.set_value([[1, 2, 3], [4, 5, 6]])
+    element_mean = OpenCVMean(title = "element_mean")
+    element_output = OutputNumber(title = "element_output")
     
     flow = Flow()
     flow.addElement(element_input)
     flow.addElement(element_mean)
     flow.addElement(element_output)
-    flow.connect(element_input.image, element_mean.src)
-    flow.connect(element_mean.mean, element_output.number)
+    flow.connect(element_input.image, element_mean.src, title = "conn_1")
+    flow.connect(element_mean.mean, element_output.number, title = "conn_2")
     flow.run()
     
     expected = 3.5
-    actual = element_output.number
+    actual = element_output.result()
     self.assertEqual(expected, actual)
   
   def test_mean_2(self):
-    element_input = InputImage([[4, 5, 6], [7, 8, 9]])
+    element_input = InputImage()
+    element_input.set_value([[4, 5, 6], [7, 8, 9]])
     element_mean = OpenCVMean()
     element_output = OutputNumber()
     
@@ -34,6 +36,6 @@ class FlowModelTests(TestCase):
     flow.run()
     
     expected = 6.5
-    actual = element_output.number
+    actual = element_output.result()
     self.assertEqual(expected, actual)
-  
+    
