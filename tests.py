cd3e419042dbe341b16c1261d31979d80c087782
Pieter-Jan Busschaert <pieterjan.busschaert+github@gmail.com>
2013-03-02 15:50:31 +0100

    Initial commit with first passing test
    Initial test is very simple flow:
    input_array => cv2.mean() => output_number
    The test is made to pass with the least amount of code (ie: it doesn't
    really call cv2.mean() yet)

---
+++ tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from improcflow.models import *
+
+class FlowModelTests(TestCase):
+    def test_mean(self):
+      element_input = InputImage([[1, 2, 3], [4, 5, 6]])
+      element_mean = OpenCVMean()
+      element_output = OutputNumber()
+      
+      flow = Flow()
+      flow.addElement(element_input)
+      flow.addElement(element_mean)
+      flow.addElement(element_output)
+      flow.connect(element_input.image, element_mean.src)
+      flow.connect(element_mean.mean, element_output.number)
+      flow.run()
+      
+      expected = 3.5
+      actual = element_output.number
+      self.assertEqual(expected, actual)
