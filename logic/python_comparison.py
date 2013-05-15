7953538f65fe9be005a3e09b51b1310ebf399034
Pieter-Jan Busschaert <pieterjan.busschaert+github@gmail.com>
2013-05-15 17:49:59 +0200

    Python comparison operators: !=

--- logic/python_comparison.py
+++ logic/python_comparison.py
@@ -13,3 +13,18 @@ class PythonIsEqualTo(Element):
     self.result.set_value(self.left.value == self.right.value)
     
 register_element_type(PythonIsEqualTo)
+
+
+class PythonIsNotEqualTo(Element):
+  class_name = "python_is_not_equal_to"
+  
+  def __init__(self, title = None, element_model = None):
+    super(PythonIsNotEqualTo, self).__init__(title = title, element_model = element_model)
+    self.left = self.add_input_connector(title = "left")
+    self.right = self.add_input_connector(title = "right")
+    self.result = self.add_output_connector(title = "result")
+  
+  def run(self):
+    self.result.set_value(self.left.value != self.right.value)
+    
+register_element_type(PythonIsNotEqualTo)
--- tests/test_python_comparison.py
+++ tests/test_python_comparison.py
@@ -49,3 +49,49 @@ class PythonComparisonTests(TestCase):
     element_input_2.set_value(0)
     flow.run()
     self.assertTrue(element_output.result())
+
+  def test_inequal_integers(self):
+    element_input_1 = InputData()
+    element_input_2 = InputData()
+    element_notequal = PythonIsNotEqualTo()
+    element_output = OutputData()
+    
+    flow = Flow()
+    flow.add_element(element_input_1)
+    flow.add_element(element_input_2)
+    flow.add_element(element_notequal)
+    flow.add_element(element_output)
+    flow.connect(element_input_1.data, element_notequal.left)
+    flow.connect(element_input_2.data, element_notequal.right)
+    flow.connect(element_notequal.result, element_output.data)
+    
+    element_input_1.set_value(3)
+    element_input_2.set_value(5)
+    flow.run()
+    self.assertTrue(element_output.result())
+    
+    element_input_1.set_value(3)
+    element_input_2.set_value(3)
+    flow.run()
+    self.assertFalse(element_output.result())
+
+    element_input_1.set_value(3)
+    element_input_2.set_value(-5)
+    flow.run()
+    self.assertTrue(element_output.result())
+
+    element_input_1.set_value(-5)
+    element_input_2.set_value(-5)
+    flow.run()
+    self.assertFalse(element_output.result())
+    
+    element_input_1.set_value(-8)
+    element_input_2.set_value(-5)
+    flow.run()
+    self.assertTrue(element_output.result())
+    
+    element_input_1.set_value(0)
+    element_input_2.set_value(0)
+    flow.run()
+    self.assertFalse(element_output.result())
+    
