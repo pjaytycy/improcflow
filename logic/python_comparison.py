9663c104dda7ee4f1c6f200e3a06fd6a2d8432d3
Pieter-Jan Busschaert <pieterjan.busschaert+github@gmail.com>
2013-05-15 17:55:34 +0200

    refactor python_comparison.py to reduce duplication

--- logic/python_comparison.py
+++ logic/python_comparison.py
@@ -1,13 +1,16 @@
 from improcflow.logic import Element, register_element_type
 
-class PythonIsEqualTo(Element):
-  class_name = "python_is_equal_to"
-  
+
+class PythonComparisonBase(Element):
   def __init__(self, title = None, element_model = None):
-    super(PythonIsEqualTo, self).__init__(title = title, element_model = element_model)
+    super(PythonComparisonBase, self).__init__(title = title, element_model = element_model)
     self.left = self.add_input_connector(title = "left")
     self.right = self.add_input_connector(title = "right")
     self.result = self.add_output_connector(title = "result")
+  
+
+class PythonIsEqualTo(PythonComparisonBase):
+  class_name = "python_is_equal_to"
   
   def run(self):
     self.result.set_value(self.left.value == self.right.value)
@@ -15,14 +18,8 @@ class PythonIsEqualTo(Element):
 register_element_type(PythonIsEqualTo)
 
 
-class PythonIsNotEqualTo(Element):
+class PythonIsNotEqualTo(PythonComparisonBase):
   class_name = "python_is_not_equal_to"
-  
-  def __init__(self, title = None, element_model = None):
-    super(PythonIsNotEqualTo, self).__init__(title = title, element_model = element_model)
-    self.left = self.add_input_connector(title = "left")
-    self.right = self.add_input_connector(title = "right")
-    self.result = self.add_output_connector(title = "result")
   
   def run(self):
     self.result.set_value(self.left.value != self.right.value)
