07352c644de43379171401e588735d2ad781003b
Pieter-Jan Busschaert <pieterjan.busschaert+github@gmail.com>
2013-05-22 10:54:11 +0200

    test: control signal true allows execution of subflow
    + refactor test_python_subflow.py to use setUp() for repetitive tasks
    + update TODO list with a few extra tasks

--- tests/test_python_subflow.py
+++ tests/test_python_subflow.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from improcflow.logic import *
 
 class PythonSubFlowTests(TestCase):
-  def test_simple_sub_flow(self):
+  def setUp(self):
     sub_flow_input_1 = InputData(title = "term1")
     sub_flow_input_2 = InputData(title = "term2")
     sub_flow_sum = PythonAddition()
@@ -20,22 +20,39 @@ class PythonSubFlowTests(TestCase):
     sub_flow.connect(sub_flow_input_2.data, sub_flow_sum.term2)
     sub_flow.connect(sub_flow_sum.sum, sub_flow_output.data)
     
+    self.sub_flow = sub_flow
     
-    element_input_1 = InputData()
-    element_input_2 = InputData()
-    element_output = OutputData()
+    self.element_input_1 = InputData()
+    self.element_input_2 = InputData()
+    self.element_output = OutputData()
     
-    flow = Flow(title = "main_flow")
-    flow.add_element(element_input_1)
-    flow.add_element(element_input_2)
-    flow.add_element(sub_flow)
-    flow.add_element(element_output)
-    flow.connect(element_input_1.data, sub_flow.term1)
-    flow.connect(element_input_2.data, sub_flow.term2)
-    flow.connect(sub_flow.sum, element_output.data)
+    self.flow = Flow(title = "main_flow")
+    self.flow.add_element(self.element_input_1)
+    self.flow.add_element(self.element_input_2)
+    self.flow.add_element(self.sub_flow)
+    self.flow.add_element(self.element_output)
+    self.flow.connect(self.element_input_1.data, self.sub_flow.term1)
+    self.flow.connect(self.element_input_2.data, self.sub_flow.term2)
+    self.flow.connect(self.sub_flow.sum, self.element_output.data)
+
+    self.element_input_1.set_value(3)
+    self.element_input_2.set_value(5)
     
-    element_input_1.set_value(3)
-    element_input_2.set_value(5)
-    flow.run()
+  
+  def test_simple_sub_flow(self):
+    self.flow.run()
     
-    self.assertEqual(8, element_output.result())
+    self.assertEqual(8, self.element_output.result())
+
+  def test_control_signal_true_allows_execution(self):
+    element_bool = InputData()
+    element_bool.set_value(True)
+    self.flow.add_element(element_bool)
+    self.flow.connect(element_bool.data, self.sub_flow.flow_control)
+    self.flow.run()
+    
+    self.assertEqual(8, self.element_output.result())
+    
+    
+    
+    
